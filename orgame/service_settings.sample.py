aac21288a04fb5424b3cbdb22655073363f40506
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-10-08 00:20:33 +0900

    Add service settings sample

---
+++ orgame/service_settings.sample.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+db_flag = False
+
+DB = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+FACEBOOK_CLIENT_ID = ''
+FACEBOOK_CLIENT_SECRET = ''
+SECRET_KEY = ''
