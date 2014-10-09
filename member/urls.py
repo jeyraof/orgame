03fe5f3fc302c2810018d3fc364bf50b1c9d6b06
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-10-10 06:28:10 +0900

    Add settings url and name views name.

--- member/urls.py
+++ member/urls.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 from django.views.generic.base import RedirectView
+from member.views import SettingsView
 
 
 urlpatterns_sign = patterns('',
                             url(r'^$', RedirectView.as_view(pattern_name='member.views.sign_in', permanent=False)),
-                            url(r'^in$', 'member.views.sign_in'),
-                            url(r'^out$', 'member.views.sign_out'),
-                            url(r'^fb/$', 'member.views.oauth_facebook'),
-                            url(r'^fb/auth$', 'member.views.oauth_facebook_auth'),
+                            url(r'^in$', 'member.views.sign_in', name='signin'),
+                            url(r'^out$', 'member.views.sign_out', name='signout'),
+                            url(r'^fb/$', 'member.views.oauth_facebook', name='facebook'),
+                            url(r'^fb/auth$', 'member.views.oauth_facebook_auth', name='facebook_oauth'),
                             )
+
+urlpatterns_settings = patterns('',
+                                url(r'^$', login_required(SettingsView.as_view()), name='settings'),
+                                )
--- orgame/urls.py
+++ orgame/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from member.urls import urlpatterns_sign
+from member.urls import urlpatterns_sign, urlpatterns_settings
 from .views import main
 
 urlpatterns = patterns('',
-                       url(r'^$', main),
+                       url(r'^$', main, name='main'),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^sign/', include(urlpatterns_sign)),
+                       url(r'^settings/', include(urlpatterns_settings))
                        )
