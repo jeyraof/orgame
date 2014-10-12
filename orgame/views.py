674ee1f8f0a9b13380bc534ce0824fa4250f83c0
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-10-13 00:48:51 +0900

    Add user search page

--- orgame/views.py
+++ orgame/views.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from member.models import Profile
+from series.models import Series
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import View
 
 
 def main(request):
@@ -8,3 +12,23 @@ def main(request):
 
 def handler404(request):
     return render(request, '404.html')
+
+
+class SearchView(View):
+    def get(self, request):
+        opt = {
+            'is_search_page': True,
+            'search_type': request.GET.get('type', 'Series')
+        }
+
+        search_keyword = request.GET.get('q', None)
+        if search_keyword:
+            profile = Profile.objects.filter(Q(nickname__contains=search_keyword)).exclude(email='')
+            opt['profile'] = profile
+            opt['profile_count'] = profile.count()
+
+            series = Series.objects.filter(Q(name__contains=search_keyword))
+            opt['series'] = series
+            opt['series_count'] = series.count()
+
+        return render(request, 'search.html', opt)
