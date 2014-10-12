25d1d5ee2fa33aed586c9281dd14a747ee2ea758
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-10-13 00:49:16 +0900

    Add search_keyword to context processor

--- orgame/context_processor.py
+++ orgame/context_processor.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 def global_context(request):
+    search_keyword = request.GET.get('q', '')
+
     processor = {
         'request': request,
         'user': request.user,
+        'search_keyword': search_keyword,
     }
 
     return processor
